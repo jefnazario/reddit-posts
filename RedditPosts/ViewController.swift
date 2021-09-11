90e464e053c37af1696249585d293692bdbc5457
Jeferson F Nazario <jefnazario@gmail.com>
2021-09-11 07:24:07 -0300

    Project creation

---
+++ RedditPosts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditPosts
+//
+//  Created by Jeferson Fracalossi Nazario on 11/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
