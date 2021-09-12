e2e5f0af2bdeb082023cc85c8668d9048b0d34f8
Jeferson F Nazario <jefnazario@gmail.com>
2021-09-12 06:38:48 -0300

    Initial UI development for testing API service
    - Added a presenter to handle UI data
    - Added a UITableView to show data
    - Created a PostsViewProtocol to presenter communicate with the UIViewController

--- RedditPosts/Posts/UI/PostsViewController.swift
+++ RedditPosts/Posts/UI/PostsViewController.swift
@@ -7,23 +7,60 @@
 
 import UIKit
 
+protocol PostsViewProtocol: AnyObject {
+    func reloadScreen()
+}
+
 class PostsViewController: UIViewController {
+    // MARK: - Instantiate lazy var
+    private lazy var api: ApiProtocol = {
+        return Api()
+    }()
+    
+    private lazy var presenter: PostsPresenterProtocol = {
+        return PostsPresenter(view: self)
+    }()
+    
+    private lazy var worker: PostsWorkerProtrocol = {
+        return PostsWorker(api: self.api, presenter: self.presenter)
+    }()
+    
+    private lazy var service: PostServiceProtocol = {
+        return PostService(worker: self.worker)
+    }()
+    
+    // MARK: - IBOutlet properties
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emptyMessage: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        service.getPosts()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func reload() {
+        service.getPosts()
     }
-    */
+}
 
+extension PostsViewController: PostsViewProtocol {
+    func reloadScreen() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension PostsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: presenter.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = presenter.posts[indexPath.row].title
+        
+        return cell
+    }
 }
